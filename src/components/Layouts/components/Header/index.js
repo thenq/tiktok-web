6f657516e7d410ca0fa29b6a33c54b8bce22031a
thenq <thenq>
2022-05-29 21:49:56 +0700

    Create ui header

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,12 +1,32 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
 function Header() {
   return (
     <div className={cx('wrapper')}>
-      <div className={cx('inner')}></div>
+      <div className={cx('inner')}>
+        <img src={images.logo} alt="logo" />
+
+        <div className={cx('search')}>
+          <input placeholder="Search account and video" spellcheck={false} />
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <button className={cx('loading')}>
+            <FontAwesomeIcon icon={faSpinner} />
+          </button>
+          <button className={cx('search-button')}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+
+        <div className={cx('actions')}></div>
+      </div>
     </div>
   );
 }
